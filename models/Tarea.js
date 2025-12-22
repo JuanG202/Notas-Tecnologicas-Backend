70da853bf9da7886e4332873066635be0339ac4d
kevin <kevinr060799@gmail.com>
2025-12-22 16:51:09 -0500

    Se deja funcionando el login y el register

--- models/Tarea.js
+++ models/Tarea.js
@@ -3,14 +3,14 @@ const mongoose = require("mongoose");
 const TareaSchema = new mongoose.Schema({
   titulo: String,
   descripcion: String,
+   tecnico: { type: String, default: "" },
   completada: { type: Boolean, default: false },
   usuario: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: "Usuario",
-    required: true
+    ref: "Usuario"
   },
   fechaCreacion: { type: Date, default: Date.now },
-  fechaActualizacion: Date
+  fechaActualizacion: { type: Date }
 });
 
 module.exports = mongoose.model("Tarea", TareaSchema);
--- routes/tareas.js
+++ routes/tareas.js
@@ -56,33 +56,23 @@ router.put("/:id", auth, async (req, res) => {
 /* =========================
    DELETE - ELIMINAR TAREA
 ========================= */
-router.delete("/:id", auth, async (req, res) => {
+router.patch("/:id/toggle", auth, async (req, res) => {
   try {
-    const tarea = await Tarea.findByIdAndDelete(req.params.id);
+    const tarea = await Tarea.findById(req.params.id);
 
     if (!tarea) {
       return res.status(404).json({ mensaje: "Tarea no encontrada" });
     }
 
-    res.json({ mensaje: "Tarea eliminada" });
+    tarea.completada = !tarea.completada;
+    tarea.fechaActualizacion = new Date();
+
+    await tarea.save();
+    res.json(tarea);
   } catch (error) {
-    res.status(500).json({ mensaje: "Error al eliminar tarea" });
+    console.error(error);
+    res.status(500).json({ mensaje: "Error al cambiar estado" });
   }
-});
-
-
-router.patch("/:id/toggle", auth, async (req, res) => {
-  const tarea = await Tarea.findById(req.params.id);
-
-  if (!tarea) {
-    return res.status(404).json({ mensaje: "Tarea no encontrada" });
-  }
-
-  tarea.completada = !tarea.completada;
-  tarea.fechaActualizacion = new Date();
-  await tarea.save();
-
-  res.json(tarea);
 });
 
 
